c80c9bfe5e6990e931af963c556fcc2b796d8636
Mudit Sharma <115790300+mudit203@users.noreply.github.com>
2024-09-20 11:26:00 +0530

    Update Founders.jsx

--- src/components/Home/Proshows/Founders.jsx
+++ src/components/Home/Proshows/Founders.jsx
@@ -4,7 +4,7 @@ const Founders = () => {
     return (
       <div id="founders" className="flex flex-col  text-white items-center px-5 stretch-to-screen mt-8">
         <div className="main text-6xl md:7xl mb-5 text-white">
-          <span className="socod text-left">OUR FOUNDERS</span>
+          <span className="font-bold text-5xl capitalize text-white">OUR FOUNDERS</span>
         </div>
         <div className="proshow-cards">
         <div className="proshow-card relative flex flex-col justify-end items-center rounded-xl">
@@ -61,4 +61,8 @@ const Founders = () => {
   
   
   export default Founders;
+  
+
+  
+
   
