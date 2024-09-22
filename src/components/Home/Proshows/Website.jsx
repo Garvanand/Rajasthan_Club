26c8c31497900abc4324e8bc002002b3222443e1
Garvanand <33595457+Garvanand@users.noreply.github.com>
2024-09-23 02:23:44 +0530

    team updated

--- src/components/Home/Proshows/Website.jsx
+++ src/components/Home/Proshows/Website.jsx
@@ -1,60 +1,80 @@
-/* import "./Proshow.css"; */
+  /* import "./Proshow.css"; */
 
-const Website = () => {
-    return (
-      <div id="spotlight" className="flex flex-col  text-white items-center px-5 stretch-to-screen mt-8">
-        <div className="main text-6xl md:7xl mb-5 text-white">
-          <span className="font-bold text-5xl capitalize text-white">DIGITAL PRESENCE MANAGER</span>
-        </div>
-        <div className="proshow-cards">
-        <div className="proshow-card relative flex flex-col justify-end items-center rounded-xl">
-            <img
-              className="-z-10 absolute proshows-bg rounded-xl"
-              alt="background-raina"
-              src={require("../../../assets/bg-2.webp")}
-            />
-            <img
-              src={require("../../../assets/garv.webp")}
-              alt="garv"
-              className="z-0 rounded-xl"
-            />
-            <div className="z-10  flex flex-col absolute my-4">
-              <h1 className="  text-3xl text-center font-bold tracking-widest content">
-                GARV ANAND
-              </h1>
-              <h1 className="  text-md text-center font-bold tracking-widest content">
-                HEAD
-              </h1>
-            </div>
+  const Website = () => {
+      return (
+        <div id="spotlight" className="flex flex-col  text-white items-center px-5 stretch-to-screen mt-8">
+          <div className="main text-6xl md:7xl mb-5 text-white">
+            <span className="font-bold text-5xl capitalize text-white">DIGITAL PRESENCE MANAGER</span>
           </div>
+          <div className="proshow-cards">
           <div className="proshow-card relative flex flex-col justify-end items-center rounded-xl">
-            <img
-              className="-z-10 absolute proshows-bg rounded-xl"
-              alt="background-raina"
-              src={require("../../../assets/bg-2.webp")}
-            />
-            <img
-              src={require("../../../assets/mudit.webp")}
-              alt="garv"
-              className="z-0 rounded-xl"
-            />
-            <div className="z-10  flex flex-col absolute my-4">
-              <h1 className="  text-3xl text-center font-bold tracking-widest content">
-                MUDIT SHARMA
-              </h1>
-              <h1 className="  text-md text-center font-bold tracking-widest content">
-                MEMBER
-              </h1>
+              <img
+                className="-z-10 absolute proshows-bg rounded-xl"
+                alt="background-raina"
+                src={require("../../../assets/bg-2.webp")}
+              />
+              <img
+                src={require("../../../assets/garv.webp")}
+                alt="garv"
+                className="z-0 rounded-xl"
+              />
+              <div className="z-10  flex flex-col absolute my-4">
+                <h1 className="  text-3xl text-center font-bold tracking-widest content">
+                  GARV ANAND
+                </h1>
+                <h1 className="  text-md text-center font-bold tracking-widest content">
+                  HEAD
+                </h1>
+              </div>
             </div>
+            <div className="proshow-card relative flex flex-col justify-end items-center rounded-xl">
+              <img
+                className="-z-10 absolute proshows-bg rounded-xl"
+                alt="background"
+                src={require("../../../assets/bg-2.webp")}
+              />
+              <img
+                src={require("../../../assets/mudit.webp")}
+                alt="mudit"
+                className="z-0 rounded-xl"
+              />
+              <div className="z-10  flex flex-col absolute my-4">
+                <h1 className="  text-3xl text-center font-bold tracking-widest content">
+                  MUDIT SHARMA
+                </h1>
+                <h1 className="  text-md text-center font-bold tracking-widest content">
+                  MEMBER
+                </h1>
+              </div>
+            </div>
+            <div className="proshow-card relative flex flex-col justify-end items-center rounded-xl">
+              <img
+                className="-z-10 absolute proshows-bg rounded-xl"
+                alt="background-raina"
+                src={require("../../../assets/bg-2.webp")}
+              />
+              <img
+                src={require("../../../assets/vansh.webp")}
+                alt="garv"
+                className="z-0 rounded-xl"
+              />
+              <div className="z-10  flex flex-col absolute my-4">
+                <h1 className="  text-3xl text-center font-bold tracking-widest content">
+                  VANSH KASHYAP
+                </h1>
+                <h1 className="  text-md text-center font-bold tracking-widest content">
+                  MEMBER
+                </h1>
+              </div>
+            </div>
+            
           </div>
-          
         </div>
-      </div>
+        
+      );
       
-    );
+    };
     
-  };
-  
-  
-  export default Website;
-  
+    
+    export default Website;
+    
