1aa262e33828b765a3455daba8475fcdcc9a6b91
Garvanand <33595457+Garvanand@users.noreply.github.com>
2024-11-20 20:50:43 +0530

    team updated

--- src/components/Home/Proshows/Website.jsx
+++ src/components/Home/Proshows/Website.jsx
@@ -23,7 +23,7 @@
                   GARV ANAND
                 </h1>
                 <h1 className="  text-md text-center font-bold tracking-widest content">
-                  HEAD
+                  HEAD & CREATOR
                 </h1>
               </div>
             </div>
@@ -66,6 +66,28 @@
                   MEMBER
                 </h1>
               </div>
+              
+            </div>
+            <div className="proshow-card relative flex flex-col justify-end items-center rounded-xl">
+              <img
+                className="-z-10 absolute proshows-bg rounded-xl"
+                alt="background-raina"
+                src={require("../../../assets/bg-2.webp")}
+              />
+              <img
+                src={require("../../../assets/vishal.webp")}
+                alt="garv"
+                className="z-0 rounded-xl"
+              />
+              <div className="z-10  flex flex-col absolute my-4">
+                <h1 className="  text-3xl text-center font-bold tracking-widest content">
+                  VISHAL SONI
+                </h1>
+                <h1 className="  text-md text-center font-bold tracking-widest content">
+                  MEMBER
+                </h1>
+              </div>
+              
             </div>
             
           </div>
