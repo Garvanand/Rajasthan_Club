c55e1bd39046c8a50b1b6892ea4a93bee4dbaf63
Garvanand <33595457+Garvanand@users.noreply.github.com>
2024-11-20 20:53:16 +0530

    heading updated

--- src/components/Home/Proshows/Website.jsx
+++ src/components/Home/Proshows/Website.jsx
@@ -4,7 +4,7 @@
       return (
         <div id="spotlight" className="flex flex-col  text-white items-center px-5 stretch-to-screen mt-8">
           <div className="main text-6xl md:7xl mb-5 text-white">
-            <span className="font-bold text-5xl capitalize text-white">DIGITAL PRESENCE MANAGER</span>
+            <span className="font-bold text-5xl capitalize text-white">DIGITAL PRESENCE MANAGERS</span>
           </div>
           <div className="proshow-cards">
           <div className="proshow-card relative flex flex-col justify-end items-center rounded-xl">
